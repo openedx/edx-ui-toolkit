20f2db63364e3fcd7d417fee1e6e527a358d2ec9
Andy Armstrong <andya@edx.org>
2015-10-13 17:54:08 -0400

    Migrate LMS card views to the UI Toolkit
    UITK-11

--- components/config.js
+++ components/config.js
@@ -8,7 +8,8 @@ require.config({
     paths: {
         jquery: 'bower_components/jquery/dist/jquery',
         underscore: 'bower_components/underscore/underscore',
-        backbone: 'bower_components/backbone/backbone'
+        backbone: 'bower_components/backbone/backbone',
+        text : 'bower_components/requirejs-text/text'
     },
     wrapShim: true,
     shim: {
