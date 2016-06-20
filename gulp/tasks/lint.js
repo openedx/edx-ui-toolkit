1b644ba197b6c99d89004c89c07c7f6b59e167f3
Brian Jacobel <bjacobel@gmail.com>
2016-06-20 18:21:30 -0400

    Piping a file glob thru gulp-eslint takes 20sec? with gulp-shell it's 1sec

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,21 +1,8 @@
 (function() {
     'use strict';
 
-    var eslint = require('gulp-eslint'),
-        gulp = require('gulp'),
-        paths = {
-            lint: [
-                'test/**/!(jquery.simulate)*.js',
-                'src/**/*.js',
-                'gulp/tasks/*.js',
-                'gulpfile.js'
-            ]
-        };
+    var gulp = require('gulp'),
+        shell = require('gulp-shell');
 
-    gulp.task('lint', function() {
-        return gulp.src(paths.lint)
-            .pipe(eslint())
-            .pipe(eslint.format())
-            .pipe(eslint.failAfterError());
-    });
+    gulp.task('lint', shell.task('eslint .'));
 }());
