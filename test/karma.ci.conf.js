d0f1c08b871e64c2c940ddebb8fdbd03d9be4d3a
Usama Sadiq <usama.sadiq@arbisoft.com>
2021-10-26 19:07:38 +0500

    build: Replace Travis with GitHub CI

--- test/karma.ci.conf.js
+++ test/karma.ci.conf.js
@@ -1,6 +1,6 @@
 var baseConfig = require('./karma.conf');
 
-// Karma configuration to run in Travis
+// Karma configuration to run in CI
 module.exports = function(config) {
     'use strict';
 
@@ -8,12 +8,12 @@ module.exports = function(config) {
         singleRun: true,
         autoWatch: false,
         customLaunchers: {
-            Chrome_travis_ci: {
+            Chrome_github_ci: {
                 base: 'Chrome',
                 flags: ['--no-sandbox']
             }
         },
-        browsers: ['Chrome_travis_ci', 'Firefox', 'PhantomJS'],
+        browsers: ['Chrome_github_ci', 'Firefox', 'PhantomJS'],
         logLevel: config.LOG_INFO,
         preprocessors: {
             'src/js/**/*.js': ['coverage']
